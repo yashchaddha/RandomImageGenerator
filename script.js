0c875bcf3487ba29ac937fd88e98838c66727156
Yash Chaddha <yash.chaddha1997@gmail.com>
2023-07-03 17:24:02 +0530

    Adding a Feature in which the API is called as soon as Page loads

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+    // API request code goes here
+    $.ajax({
+      url: 'https://dog.ceo/api/breeds/image/random', // Replace with your API endpoint
+      method: 'GET', // Specify the HTTP method (GET, POST, etc.)
+      success: function(response) {
+        // Handle the API response
+        $('#dog-image').attr('src',response.message);
+      },
+      error: function(xhr, status, error) {
+        // Handle any errors that occur during the API request
+        console.error(status + ': ' + error);
+      }
+    });
+  });
+  
 function fetchRandomDogImage(){
     var xhrRequest=new XMLHttpRequest();
 
